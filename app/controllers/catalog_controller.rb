2f67f9085e04e91132fa492e7da40b9f809b1b46
leaannbradford <ltrammer@gmail.com>
2022-12-19 13:39:54 -0800

    add filesets to search for collection show page

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -40,7 +40,7 @@ class CatalogController < ApplicationController
     config.default_solr_params = {
       qt: "search",
       rows: 10,
-      qf: "title_tesim description_tesim creator_tesim keyword_tesim"
+      qf: "title_tesim description_tesim creator_tesim keyword_tesim all_text_timv"
     }
 
     # Specify which field to use in the tag cloud on the homepage.
