f287bc0f4abf9cade5f692e057df11a371119975
sensei100 <tuh42082@temple.edu>
2023-02-28 11:18:17 -0500

    Add network_mode to docker-compose file

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(*groups)
 
 module Hyku
   class Application < Rails::Application
+    config.active_record.yaml_column_permitted_classes = [ActiveSupport::HashWithIndifferentAccess]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
