dd75b810ce94e651bec06357ca2dff224e93944f
sensei100 <tuh42082@temple.edu>
2023-03-29 10:38:09 -0400

    Updates config to avoid ActiveSupport::HashWithIndifferentAccess error

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,11 @@ Bundler.require(*groups)
 
 module Hyku
   class Application < Rails::Application
-    config.active_record.yaml_column_permitted_classes = [ActiveSupport::HashWithIndifferentAccess]
+    config.active_record.yaml_column_permitted_classes = [
+      Symbol,
+      ActiveSupport::HashWithIndifferentAccess,
+      ActionController::Parameters
+    ]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
