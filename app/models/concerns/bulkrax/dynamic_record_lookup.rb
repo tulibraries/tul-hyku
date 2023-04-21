86ffbbb1edc8fa92c070d0c31c42de947dddf135
leaannbradford <ltrammer@gmail.com>
2023-04-21 13:07:45 -0700

    debugging

---
+++ app/models/concerns/bulkrax/dynamic_record_lookup.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module Bulkrax
+  # TODO: Extract methods to class methods; there's no reason for these methods to be a mixin.
+  # TODO: Add specs to test in isolation
+  module DynamicRecordLookup
+    # Search entries, collections, and every available work type for a record that
+    # has the provided identifier.
+    #
+    # @param identifier [String] Work/Collection ID or Bulkrax::Entry source_identifier
+    # @param importer_run_id [Number] ID of the current_run of this Importer Job
+    # @return [Entry, nil], [Work, Collection, nil] Entry if found, otherwise nil and a Work or Collection if found, otherwise nil
+    def find_record(identifier, importer_run_id = nil)
+      # check for our entry in our current importer first
+      importer_id = Bulkrax::ImporterRun.find(importer_run_id).importer_id
+      default_scope = { identifier: identifier, importerexporter_type: 'Bulkrax::Importer' }
+
+      begin
+        # the identifier parameter can be a :source_identifier or the id of an object
+        record = Bulkrax::Entry.find_by(default_scope.merge({ importerexporter_id: importer_id })) || Entry.find_by(default_scope)
+        record ||= ActiveFedora::Base.find(identifier)
+      # NameError for if ActiveFedora isn't installed
+      rescue NameError, ActiveFedora::ObjectNotFoundError
+        record = nil
+      end
+
+      # return the found entry here instead of searching for it again in the CreateRelationshipsJob
+      # also accounts for when the found entry isn't a part of this importer
+      record.is_a?(Bulkrax::Entry) ? [record, record.factory.find] : [nil, record]
+    end
+
+    # Check if the record is a Work
+    def curation_concern?(record)
+      available_work_types.include?(record.class)
+    end
+
+    private
+
+    # @return [Array<Class>] list of work type classes
+    def available_work_types
+      # If running in a Hyku app, do not include disabled work types
+      @available_work_types ||= if defined?(::Hyku)
+                                  ::Site.instance.available_works.map(&:constantize)
+                                else
+                                  Bulkrax.curation_concerns
+                                end
+    end
+  end
+end
