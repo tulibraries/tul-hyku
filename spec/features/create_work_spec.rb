37a133cdcf8c455c92b61aad9e22c8a35a9b58e7
Shana Moore <shana@scientist.com>
2023-10-23 12:09:57 -0700

    :bug: Depositor role cannot deposit into admin set fix
    We implemented a fix for this bug in PALS and are contributing it back to Hyku.
    
    Issue:
    - https://github.com/scientist-softserv/palni-palci/issues/864

--- spec/features/create_work_spec.rb
+++ spec/features/create_work_spec.rb
@@ -18,4 +18,54 @@ RSpec.describe 'Creating a new Work', type: :feature, clean: true do
     click_link "Share Your Work"
     expect(page).to have_button "Create work"
   end
+
+  context 'as a user with no roles' do
+    let(:user) { create(:user) }
+
+    it 'cannot see the add new work button' do
+      visit '/dashboard/my/works'
+      expect(page).not_to have_link "Add New Work"
+    end
+
+    context 'who has deposit access for a specific admin set' do
+      let(:admin_set_2) do
+        create(:admin_set, title: ["Another Admin Set"],
+                           description: ["A description"])
+      end
+
+      before do
+        create(:permission_template_access,
+               :deposit,
+               permission_template: create(:permission_template, source_id: admin_set_2.id, with_admin_set: true, with_active_workflow: true),
+               agent_type: 'user',
+               agent_id: user.user_key)
+      end
+
+      it 'can see the add new work button' do
+        visit '/dashboard/my/works'
+        expect(page).to have_link "Add New Work"
+      end
+    end
+
+    context 'who belongs to a group with deposit access for a specific admin set' do
+      let(:admin_set_3) do
+        create(:admin_set, title: ["Yet Another Admin Set"],
+                           description: ["A description"])
+      end
+      let(:depositors_group) { create(:depositors_group, name: 'deposit', member_users: [user]) }
+
+      before do
+        create(:permission_template_access,
+               :deposit,
+               permission_template: create(:permission_template, source_id: admin_set_3.id, with_admin_set: true, with_active_workflow: true),
+               agent_type: 'group',
+               agent_id: depositors_group.name)
+      end
+
+      it 'can see the add new work button' do
+        visit '/dashboard/my/works'
+        expect(page).to have_link "Add New Work"
+      end
+    end
+  end
 end
