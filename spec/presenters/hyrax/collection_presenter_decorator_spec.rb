d2649f93e3316d70c534e58403c7cb77705fec05
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-12-19 13:56:57 -0500

    🎁 Contribute back work from PALNI/PALCI
    Related to Issues:
    
    - https://github.com/samvera/hyku/issues/1815
    - https://github.com/scientist-softserv/palni-palci/issues/951
    
    Related to Pull Requests:
    
    - https://github.com/scientist-softserv/palni-palci/pull/952

---
+++ spec/presenters/hyrax/collection_presenter_decorator_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Hyrax::CollectionPresenter do
+  describe '#collection_type_badge' do
+    subject { presenter.collection_type_badge }
+
+    # We're decorating an alternate base class so that we don't need the full pre-amble for testing
+    # our decoration.  In other words, let's trust Hyrax::CollectionPresenter's specs for the
+    # "super" method call.
+    let(:base_class) do
+      Class.new do
+        def collection_type_badge
+          "<span>"
+        end
+        prepend Hyrax::CollectionPresenterDecorator
+      end
+    end
+    let(:presenter) { base_class.new }
+
+    before { allow(Site).to receive(:account).and_return(account) }
+
+    context 'when the Site.account is nil' do
+      let(:account) { nil }
+
+      it { is_expected.to eq("") }
+    end
+
+    context 'when the Site.account is search_only' do
+      let(:account) { FactoryBot.build(:account, search_only: true) }
+
+      it { is_expected.to eq("") }
+    end
+
+    context 'when the Site.account is NOT search_only' do
+      let(:account) { FactoryBot.build(:account, search_only: false) }
+
+      it { is_expected.to start_with("<span") }
+    end
+
+    context 'super_method' do
+      subject { Hyrax::CollectionPresenter.instance_method(:collection_type_badge).super_method }
+
+      let(:account) { nil }
+
+      it 'is Hyrax::CollectionPresenter#collection_type_badge' do
+        expect(subject.source_location.first).to end_with("app/presenters/hyrax/collection_presenter.rb")
+      end
+    end
+  end
+end
